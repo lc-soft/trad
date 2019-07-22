1d212c91da27fc0e93837764ff0645522aba3eb1
d4yvector <hhyuga201515@gmail.com>
2019-07-22 20:13:03 +0900

    feat: add command-line interface (#3)

--- packages/trad-cli/tradc.js
+++ packages/trad-cli/tradc.js
@@ -1,7 +1,18 @@
 const { compile } = require('./index')
+const program = require('commander')
+const pjson = require('../../package.json')
 
-// FIXME: Add command-line interfaces
+program
+  .version(pjson.version)
+  .arguments('[file]')
+  .description('Compile [file] to C source file.')
+  .action(file => sourceFile = file)
 
-if (process.argv.length === 3) {
-  compile(process.argv[2])
+program.parse(process.argv)
+
+if (typeof sourceFile === 'undefined') {
+  console.error('no source file given.\n')
+  program.help()
 }
+
+compile(sourceFile)

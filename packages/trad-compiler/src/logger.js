bb8232b4ad89dd4db1de9512d0f9800f9ae91715
Liu <lc-soft@live.cn>
2019-10-11 12:29:33 +0800

    feat: friendly error output (#4) (#8)

---
+++ packages/trad-compiler/src/logger.js
@@ -0,0 +1,57 @@
+const chalk = require('chalk')
+const pluralize = require('pluralize')
+
+class Logger {
+  constructor() {
+    this.files = new Set()
+    this.records = {}
+    this.errors = 0
+    this.warnings = 0
+  }
+
+  record(record) {
+    this.files.add(record.file || 'unknown')
+    if (record.type === 'error') {
+      this.errors += 1
+    } else {
+      this.warnings += 1
+    }
+    if (this.records[record.file] instanceof Array) {
+      this.records[record.file].push(record)
+    } else {
+      this.records[record.file] = [record]
+    }
+  }
+
+  output() {
+    const total = this.errors + this.warnings
+
+    if (total < 1) {
+      return
+    }
+    console.log()
+    this.files.forEach((file) => {
+      console.log(file)
+      this.records[file].forEach((record) => {
+        const loc = `${record.location.line}:${record.location.column}`.padEnd(8, ' ')
+  
+        if (record.type === 'error') {
+          console.log(`${loc}${chalk.red('error')} ${record.message}`)
+        } else {
+          console.log(`${loc}${chalk.yellow('warning')} ${warn.message}`)
+        }
+      })
+      console.log()
+    })
+
+    const result = 
+      `✖ ${total} ${pluralize('problem', total)} (` +
+      `${this.errors} ${pluralize('error', this.errors)}, ` +
+      `${this.warnings} ${pluralize('warning', this.warnings)})`
+
+    console.log(this.errors > 0 ? chalk.red(result) : chalk.yellow(result))
+    console.log()
+  }
+}
+
+module.exports = Logger
